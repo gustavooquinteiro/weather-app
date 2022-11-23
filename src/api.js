8243d6b8a7c760e36d51f41c9a9ae1e1e1e81952
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2022-11-23 08:15:32 -0300

    add openweathermap api config

--- src/api.js
+++ src/api.js
@@ -7,3 +7,7 @@ export const geoApiOptions = {
 };
 
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_KEY = 'cbbe50a020ce95523b825d322450b73b';
+
+export const WEATHER_API_URL = 'https://api.openweathermap.org/data/2.5';
