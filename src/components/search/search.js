35c5b569ba870f787c935147fe07a9ba1bfb6c8b
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2022-11-23 07:06:08 -0300

    completing the search bar

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,8 +1,26 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
+import { GEO_API_URL, geoApiOptions } from "../../api";
 
 const Search = ({onSearchChange}) =>{
     const [search, setSearch] = useState(null)
+    const loadOptions = (inputValue) => {
+        return fetch(`${GEO_API_URL}/cities?namePrefix=${inputValue}`, geoApiOptions)
+        .then(response => response.json())
+        .then(response => {
+            return{
+                options: response.data.map((city)=>{
+                    return {
+                        value:`${city.latitude} ${city.longitude}`,
+                        label: `${city.name}, ${city.countryCode}`,
+                    }
+                })
+            }
+        })
+        .catch(err => console.error(err));
+
+    }
+
     const handleOnChange = (searchData) => {
         setSearch(searchData); 
         onSearchChange(searchData)
@@ -13,6 +31,7 @@ const Search = ({onSearchChange}) =>{
         debounceTimeout={600}
         value={search}
         onChange={handleOnChange}
+        loadOptions={loadOptions}
         />
 );
 }
