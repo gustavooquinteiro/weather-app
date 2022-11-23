2ebc7078fc45e66a53d7704c5956edc84e97a1b5
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2022-11-23 08:17:08 -0300

    display current weather on searched city

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import { WEATHER_API_KEY, WEATHER_API_URL } from './api';
 import './App.css';
+import CurrentWeather from './components/current-weather/current-weather';
+import Search from './components/search/search';
 
 function App() {
+
+  const [weather, setWeather] = useState(null)
+  const [forecast, setForecast] = useState(null)
+
+  const handleOnSearchChange = (searchData) => {
+    const [lat, lon] = searchData.value.split(" ");
+
+    const currentWeatherFetch = fetch(`${WEATHER_API_URL}/weather?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+    const forecastFetch = fetch(`${WEATHER_API_URL}/forecast?lat=${lat}&lon=${lon}&appid=${WEATHER_API_KEY}&units=metric`)
+
+    Promise.all([currentWeatherFetch, forecastFetch]).then(async (response) => {
+      const weatherResponse = await response[0].json()
+      const forecastResponse = await response[1].json()
+      
+      setWeather({city: searchData.label, ...weatherResponse})
+      setForecast({city: searchData.label, ...forecastResponse})
+    }).catch((err) => console.log(err))
+  }
+  console.log(weather)
+  console.log(forecast)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <Search onSearchChange={handleOnSearchChange} />
+      {weather && <CurrentWeather data={weather}/>}
     </div>
   );
 }
