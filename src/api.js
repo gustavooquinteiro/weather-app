b756b2037489161cec9d742a11570d2d245c6b9e
Gustavo Oliveira Quinteiro <gustavo200976@gmail.com>
2022-11-23 07:05:38 -0300

    add api constants and configs

---
+++ src/api.js
@@ -0,0 +1,9 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '01a752f3b0msh3aa53392fee6d6ap1415c4jsn23733cd3ca3a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
